8da9e069b89bb052aa2041ff2b01ed52e9d2c4b3
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2014-06-17 11:07:31 -0400

    fixed error

--- config/session.js
+++ config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again.
-  secret: '7002e754f59a0b64df83f8b8455b0a00'
+  secret: '7002e754f59a0b64df83f8b8455b0a00',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
